85d628742cbfee4799d096372f271b49d6b66a52
abu al ameen <abulyaqs@gmail.com>
2024-04-12 22:45:02 +0100

    task 3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" this class is..."""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -7,6 +9,7 @@ babel = Babel(app)
 
 
 class Config(object):
+    """this class defines"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -16,11 +19,13 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """this funciton d"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
+    """this is the index"""
     return render_template('2-index.html')
 
 if __name__ == '__main__':
